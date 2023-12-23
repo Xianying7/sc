c4af70df0ff657a0554a5b4719ade9ab3f751bd1
Xianying7 <148400946+Xianying7@users.noreply.github.com>
2023-12-23 16:01:03 +0700

    update aW5zdGFuIHBheQ.php

--- aW5zdGFuIHBheQ.php
+++ aW5zdGFuIHBheQ.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 if($eval == false){
   eval(str_replace('<?php',"",get_e("build_index.php")));
   eval(str_replace('<?php',"",get_e("shortlink_index.php")));
@@ -14,7 +13,9 @@ $web = [
   "freeltc.fun",
   "earnsolana.xyz",
   "claim88.fun",
-  "earncryptowrs.in"
+  "earncryptowrs.in",
+  "onlyfaucet.com",
+  "claimcoins.net",
   ];
   
 for($i=0;$i<count($web);$i++){
@@ -29,14 +30,18 @@ if(!$host){
   goto go_home;
 }
 
+if($host == "onlyfaucet.com" || $host == "claimcoins.net"){
+  $cancel = 1;
+}
 
 eval(str_replace('name_host',explode(".",$host)[0],str_replace('example',$host,'const host="https://example/",sc="name_host",cookie_only="cookie_example",mode="vie_free";')));
 #die(print_r(curl($url,["referer:".$referer],0,true)));
 
+
 DATA:
 $email = save("email");
-
-$r = base_run(host);
+#<a href="https://claimcoins.net/links/currency/ltc"
+$r = base_run(host);//die(print_r($r));
 if($r["status"] == 403){
   die(m."banned IP please airplane mode for a moment");
 } elseif(!$r["login"]){
@@ -54,6 +59,10 @@ print $r["notif"];r();
       
       
 home:
+if($host == "claimcoins.net"){
+  $link = "https://claimcoins.net/links/currency/ltc";
+  goto bn;
+}
 ket(1, "shortlinks", 2, "faucet");
 $pil = preg_replace("/[^0-9]/","",trim(tx("number")));
 if($pil == 1){
@@ -77,7 +86,7 @@ if(!$host){
   goto go;
 }
 $link = $r[$type][1][$p-1];
-
+bn:
 c().asci(sc).ket("email",$email,"type",$host).line();
 if($pil == 1){
   goto links;
@@ -87,7 +96,7 @@ if($pil == 1){
 
 links:
 while(true){
-  $r1 = base_run($link);
+  $r1 = base_run($link);#die(print_r($r1));
   if($r1["status"] == 403){
   die(m."banned IP please airplane mode for a moment");
   } elseif($r1["login"]){
@@ -97,7 +106,7 @@ while(true){
     print m."devnya Rungkat ganti coin aja".n;
     goto go;
   }
-  $bypass = visit_short($r1);
+  $bypass = visit_short($r1, $cancel);
   if($bypass == "refresh"){
      continue;
    } elseif(!$bypass){
@@ -108,17 +117,21 @@ while(true){
      lah(2,"shortlinks");
      goto home;
    }
+   if($host == "onlyfaucet.com" || $host == "claimcoins.net"){
+     L(25);
+   }
    $r1 = base_run($bypass);
    if($r1["login"]){
      continue;
    } 
-   if(preg_match("#suc#is",$r1["notif"])){
+   if(preg_match("#(suc|sent)#is",$r1["notif"])){
      text_line(h.$r1["notif"]);
    } else {
      print m.$r1["notif"];
      r();
    }
 }
+
 
       
 
@@ -135,18 +148,18 @@ function base_run($url, $data = 0){
   if(!$json){
     $json = $r[2];
   }
-  preg_match("#Enter Your Faucet#is",$r[1],$login);
-  preg_match("#empty<#is",$r[1],$empty);
+  preg_match("#(>Login<|Enter Your Faucet)#is",$r[1],$login);
+  preg_match("#Ready<#is",$r[1],$ready);
   preg_match_all('#<input type="hidden" name="(.*?)" id="token" value="(.*?)">#is',str_replace('name="anti','',$r[1]),$token);
   preg_match_all('#(title|html):(.*?)(,)#is',str_replace("'","",$r[1]),$nn);
-  if(preg_match_all('#<a class="collapse-item" href="(.*?)">(.*?)</a>#is',$r[1],$coin)){
-    for($i = 0;$i<=count($coin[1]);$i++){
-      if(preg_match("#(link)#is",$coin[2][$i])){
-        $links[] = $coin[1][$i];
-        $tl[] = $coin[2][$i];
+  if(preg_match_all('#<a class="(collapse-item|dropdown-item)" href="(.*?)">(.*?)</a>#is',$r[1],$coin)){
+    for($i = 0;$i<=count($coin[2]);$i++){
+      if(preg_match("#(link)#is",$coin[3][$i])){
+        $links[] = $coin[2][$i];
+        $tl[] = $coin[3][$i];
       } else {
-        $faucet[] = $coin[1][$i];
-        $tf[] = $coin[2][$i];
+        $faucet[] = $coin[2][$i];
+        $tf[] = $coin[3][$i];
       }
     }
     $methode["links"]= [array_filter($tl),array_filter($links)];  
@@ -162,16 +175,16 @@ function base_run($url, $data = 0){
   
    print p;
    return array_merge([
+     "r" => $r[0][2],
      "login" => $login[0],
-     "empty" => $empty[0],
+     "ready" => $ready[0],
      "res" => $r[1],
      "token" => $token,
      "name" => $name[1],
      "visit" => $visit[0],
      "left" => $left[1],
      "url1" => $r[0][0]["location"],
-     "notif" => $nn[2][0].$nn[2][1],
-     "status" => $r[0][1]["http_code"]
+     "notif" => $nn[2][0].$nn[2][1]
      ], $methode);
 }
 
