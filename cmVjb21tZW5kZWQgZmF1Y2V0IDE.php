8519e64e8685ccf7b5bd82bcbf6c87a13ec78360
Xianying7 <148400946+Xianying7@users.noreply.github.com>
2024-01-01 20:39:06 +0700

    update cmVjb21tZW5kZWQgZmF1Y2V0IDE.php

--- cmVjb21tZW5kZWQgZmF1Y2V0IDE.php
+++ cmVjb21tZW5kZWQgZmF1Y2V0IDE.php
@@ -1,17 +1,6 @@
 <?php
 
 
-function get_e($input) {
-    error_reporting(0);
-    while (true) {
-        $string = file_get_contents($input);
-        if (!$string) {
-            continue;
-        } else {
-            return $string;
-        }
-    }
-}
 
 if (!$eval) {
     eval(str_replace('<?php', "", get_e("build_index.php")));
