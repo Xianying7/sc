7d2db389b94d3b170cfa6194c41e697fbafbd980
Xianying7 <148400946+Xianying7@users.noreply.github.com>
2024-01-07 11:02:19 +0700

    RmF1Y2V0IExlZ2VuZCAy.php

--- RmF1Y2V0IExlZ2VuZCAy.php
+++ RmF1Y2V0IExlZ2VuZCAy.php
@@ -5,15 +5,35 @@ if (!$eval) {
     eval(str_replace('<?php', "", get_e("build_index.php")));
     eval(str_replace('<?php', "", get_e("shortlink_index.php")));
 }
+
+go:
+c();
+
+$web = [
+    "tokenmix.pro",
+    "paybits.xyz"
+];
+
+for ($i = 0; $i < count($web); $i++) {
+    if ($web[$i]) {
+        ket($i + 1, $web[$i]);
+    }
+}
+
+$p = tx("number", 1);
+$host = $web[$p - 1];
+if (!$host) {
+    goto go;
+}
 #$r = json_decode(file_get_contents("patak.html"));
-eval(str_replace('name_host',explode(".","tokenmix.pro")[0],str_replace('example',"tokenmix.pro",'const host="https://example/",sc="name_host",cookie_only="cookie_example";')));
+eval(str_replace('name_host', explode(".", $host)[0], str_replace('example', $host, 'const host="https://example/",sc="name_host",cookie_only="cookie_example";')));
 
 DATA:
 $u_a = save("useragent");
 $u_c = save(cookie_only);
 
 
-$r = base_run("https://tokenmix.pro/infos/dashboard_info",1);
+$r = base_run(host . "infos/dashboard_info",1);
 
 c() . asci(sc).ket("email", $r["email"]);
 ket("balance", $r["coins"], "points", $r["activity_points"]);
@@ -23,7 +43,7 @@ print n;
 
 while (true) {
     $data = json_encode(["page" => "sl"]);
-    $r = base_run("https://tokenmix.pro/infos/auth_page_info", $data, 1);
+    $r = base_run(host . "infos/auth_page_info", $data, 1);
     
     if ($r["status"] == 403) {
         print m . sc . " cloudflare!" . n;
@@ -50,7 +70,7 @@ while (true) {
     if ($r1["notif"]) {
         print h . $r1["notif"];
         r();
-        $r3 = base_run("https://tokenmix.pro/infos/dashboard_info", 1);
+        $r3 = base_run(host . "infos/dashboard_info", 1);
         
         if ($r3["status"] == 403) {
             print m . sc . " cloudflare!" . n;
@@ -96,13 +116,12 @@ function base_run($url, $data = 0, $json = 0) {
 
 function h_tk($json = 0) {
     global $u_a,$u_c;
-    $header[] = 'host: tokenmix.pro';
     $header[] = 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
     if ($json) {
        $header[] = 'content-type: application/json';
     }
     $header[] = 'user-agent: '.$u_a;
-    $header[] = 'referer: https://tokenmix.pro/dashboard/sl';
+    $header[] = 'referer: '.host;
     $header[] = 'cookie: '.$u_c;
     return $header;
 }
@@ -124,7 +143,7 @@ function bypass_shortlinks_tokenmix($r) {
     for ($i = 0; $i < count($sls); $i++) {
         $views = $sls[$i]->views24Hours;
         $_id = $sls[$i]->_id;
-        $name = $sls[$i]->name;
+        $name = remove_emoji($sls[$i]->name);
         $reward = $sls[$i]->reward;
         $completed = $sls[$i]->completed;
         $reset[] = $completed->reset_in;
@@ -149,7 +168,7 @@ function bypass_shortlinks_tokenmix($r) {
     ];
     upload:
     $data = json_encode(["slId" => $_id, "vote" => null, "type" => null]);
-    $js = base_run("https://tokenmix.pro/user/generateSl", $data, 1)["json"];
+    $js = base_run(host . "user/generateSl", $data, 1)["json"];
 
     if ($js->success == true) {
         ket_line("", $name, "left", $views - $completed->times."/".$views);
@@ -187,3 +206,5 @@ function countdown_array($array) {
 
     return round(min($countdown) / 1000);
 }
+
+
