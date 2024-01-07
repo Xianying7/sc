d9a299d531b4780267eb94281504f3f69e4ca6f2
Xianying7 <148400946+Xianying7@users.noreply.github.com>
2024-01-07 14:37:43 +0700

    update RmF1Y2V0IExlZ2VuZCAy.php

--- RmF1Y2V0IExlZ2VuZCAy.php
+++ RmF1Y2V0IExlZ2VuZCAy.php
@@ -25,7 +25,7 @@ $host = $web[$p - 1];
 if (!$host) {
     goto go;
 }
-#$r = json_decode(file_get_contents("patak.html"));
+
 eval(str_replace('name_host', explode(".", $host)[0], str_replace('example', $host, 'const host="https://example/",sc="name_host",cookie_only="cookie_example";')));
 
 DATA:
@@ -34,13 +34,12 @@ $u_c = save(cookie_only);
 
 
 $r = base_run(host . "infos/dashboard_info",1);
-
-c() . asci(sc).ket("email", $r["email"]);
-ket("balance", $r["coins"], "points", $r["activity_points"]);
+c() . asci(sc).ket("email", $r["json"]->user->email);
+ket("balance", $r["json"]->user->coins, "points", $r["json"]->user->activity_points);
 line();
 print n;
 
-
+sl:
 while (true) {
     $data = json_encode(["page" => "sl"]);
     $r = base_run(host . "infos/auth_page_info", $data, 1);
@@ -53,7 +52,10 @@ while (true) {
         print m . sc . " cookie expired!" . n;
         unlink(cookie_only);
         goto DATA;
+    } elseif ($r["json"]->success == false) {
+        continue;
     }
+
 
     $bypass = bypass_shortlinks_tokenmix($r["json"]);
 
@@ -61,8 +63,10 @@ while (true) {
         continue;
     } elseif ($bypass["end"] == 1) {
         $countdown = countdown_array($bypass["reset"]);
-        tmr(2,  $countdown - time());
-        continue;
+        if (preg_match("#tokenmix.pro#is",host)) {
+            goto achievements;
+        }
+        goto auto;
     }
 
     $r1 = base_run($bypass["link"]);
@@ -82,20 +86,117 @@ while (true) {
             goto DATA;
         }
         ket("reward", $bypass["reward"]);
-        ket("balance", $r3["coins"], "points", $r3["activity_points"]);
+        ket("balance", $r3["json"]->user->coins, "points", $r3["json"]->user->activity_points);
         line();
     }
 }
+
+achievements:
+$data = json_encode(["page" => "achievements"]);
+$r = base_run(host . "infos/auth_page_info", $data, 1);
+
+if ($r["status"] == 403) {
+    print m . sc . " cloudflare!" . n;
+    unlink(cookie_only);
+    goto DATA;
+} elseif ($r["login"]) {
+    print m . sc . " cookie expired!" . n;
+    unlink(cookie_only);
+    goto DATA;
+} elseif ($r["json"]->success == false) {
+    goto achievements;
+}
+
+
+if ($r["json"]->success == true) {
+    $achievements = $r["json"]->achievements;
+
+    for ($i = 0; $i < count($achievements); $i++) {
+        if ($achievements[$i]->user->completed == false) {
+            $data = json_encode(["id" => $achievements[$i]->_id]);
+            $js = base_run(host . "user/claim_achievement", $data, 1)["json"];
+
+            if ($js->success == true) {
+                $r1 = base_run(host . "infos/dashboard_info", 1);
+
+                if ($r1["status"] == 403) {
+                    print m . sc . " cloudflare!" . n;
+                    unlink(cookie_only);
+                    goto DATA;
+                } elseif ($r1["login"]) {
+                    print m . sc . " cookie expired!" . n;
+                    unlink(cookie_only);
+                    goto DATA;
+                }
+
+                text_line($js->res);
+                ket("reward", $achievements[$i]->reward);
+                ket("balance", $r1["json"]->user->coins, "points", $r1["json"]->user->activity_points);
+                line();
+            }
+        }
+    }
+}
+
+auto:
+$data = json_encode(["page" => "autofaucet"]);
+$r = base_run(host . "infos/auth_page_info", $data, 1);
+
+if ($r["status"] == 403) {
+    print m . sc . " cloudflare!" . n;
+    unlink(cookie_only);
+    goto DATA;
+} elseif ($r["login"]) {
+    print m . sc . " cookie expired!" . n;
+    unlink(cookie_only);
+    goto DATA;
+} elseif ($r["json"]->success == false) {
+    goto auto;
+}
+
+if (preg_match("#tokenmix.pro#is",host)) {
+    $coins = "Ltc";
+} else {
+    $coins = "ltc";
+}
+
+$data = json_encode(["coins" => [$coins], "mode" => "multi", "method" => "balance", "boost" => "1"]);
+$js = base_run(host . "user/create_autosession", $data, 1)["json"];
+
+if ($js->success == true) {
+    print h . $js->res;
+    r();
+
+    while (true) {
+       if (1 >= $countdown - time()) {
+            goto sl;
+        }
+        L(60);
+        $r1 = base_run(host . "user/autofaucet", 1);
+
+        if ($r1["reward"]) {
+            text_line($r1["reward"]);
+            ket("balance", $r1["balance"]);
+            line();
+        }
+    }
+}
+
+
+
+
 
 
 function base_run($url, $data = 0, $json = 0) {
     $header = h_tk($json);
     $r = curl($url, $header, $data, true, false);
     unset($header);
-    $js = $r[2]->user;
     #$r[1] = file_get_contents("bitmun.html");
     #die(file_put_contents("asu.html",$r[1]));
+    
     preg_match("#(please login again)#is", $r[1], $login);
+    preg_match('#<i class="fas fa-coins"></i>(.*?)<#is', $r[1], $balance);
+    preg_match('#<div class="AutoACell AAC-success">(.*?)</div>#is', $r[1], $reward);
     if (preg_match("#succes#is", $r[0][0]["location"])) {
        $notif = "successfully";
     }
@@ -104,14 +205,12 @@ function base_run($url, $data = 0, $json = 0) {
         "status" => $r[0][1]["http_code"],
         "res" => $r[1],
         "json" => $r[2],
-        "email" => $js->email,
-        "coins" => $js->coins,
-        "login" => $login,
-        "activity_points" => $js->activity_points,
+        "balance" => ltrim(rtrim($balance[1])),
+        "reward" => ltrim(rtrim(strip_tags($reward[1]))),
+        "login" => $login[0],
         "notif" => $notif
     ];
 }
-
 
 
 function h_tk($json = 0) {
@@ -171,6 +270,8 @@ function bypass_shortlinks_tokenmix($r) {
     $js = base_run(host . "user/generateSl", $data, 1)["json"];
 
     if ($js->success == true) {
+        print h . $js->res;
+        r();
         ket_line("", $name, "left", $views - $completed->times."/".$views);
         ket("", k . $js->shortenedUrl).line();
 
@@ -180,12 +281,13 @@ function bypass_shortlinks_tokenmix($r) {
             if (preg_match("#(http)#is", $bypass_shortlinks)) {
                 return [
                     "link" => str_replace("http:", "https:", $bypass_shortlinks),
-                    "reward" => $reward,
-                    "reset" => $reset
+                    "reward" => $reward
                 ];
             }
         }
     } else {
+        print m . $js->res;
+        r();
         return "refresh";
     }
 }
@@ -206,5 +308,3 @@ function countdown_array($array) {
 
     return round(min($countdown) / 1000);
 }
-
-
